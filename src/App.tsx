01dc33870c9b60e736ebd55999bf94cf38e2c166
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2024-03-14 21:32:46 +0200

    Show full address of counter

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
         <div className="Card">
           <b>Counter Address</b>
-          <div className="Hint">{address?.slice(0, 30) + "..."}</div>
+          <div className="Hint">{address}</div>
         </div>
 
         <div className="Card">
